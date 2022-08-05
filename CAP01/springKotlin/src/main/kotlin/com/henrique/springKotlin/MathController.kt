311fd5658a51dc28fda39b2b44b3938f8a98d617
HenriqueBros <hmbroseghini@hotmail.com>
2022-08-05 15:40:34 -0300

    secao06-squareRoot

--- CAP01/springKotlin/src/main/kotlin/com/henrique/springKotlin/MathController.kt
+++ CAP01/springKotlin/src/main/kotlin/com/henrique/springKotlin/MathController.kt
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
+import kotlin.math.sqrt
 
 @RestController
 class MathController {
 
     val counter: AtomicLong = AtomicLong()
+
+    // Soma
 
     @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
     fun sum(@PathVariable(value="numberOne") numberOne: String?,
@@ -18,19 +21,6 @@ class MathController {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo))
             throw UnsupportedMathOperationException("Please set a numeric value")
         return convertToDouble(numberOne) + convertToDouble(numberTwo)
-    }
-
-    private fun convertToDouble(strNumber: String?): Double {
-        if (strNumber.isNullOrBlank()) return 0.0
-        // BR 10,20 US 10.20
-        val number = strNumber.replace(",".toRegex(), ".")
-        return if (isNumeric(number)) number.toDouble() else 0.0
-    }
-
-    private fun isNumeric(strNumber: String?): Boolean {
-        if (strNumber.isNullOrBlank()) return false;
-        val number = strNumber.replace(",".toRegex(), ".")
-        return number.matches("""[-+]?[0-9]*\.?[0-9]+""".toRegex())
     }
 
     // Subtração
@@ -53,5 +43,25 @@ class MathController {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo))
             throw UnsupportedMathOperationException("Please set a numeric value")
         return convertToDouble(numberOne) * convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/squareRoot/{number}"])
+    fun multi(@PathVariable(value="number") number: String?
+    ) : Double {
+        if (!isNumeric(number))
+            throw UnsupportedMathOperationException("Please set a numeric value")
+        return sqrt(convertToDouble(number))
+    }
+    private fun convertToDouble(strNumber: String?): Double {
+        if (strNumber.isNullOrBlank()) return 0.0
+        // BR 10,20 US 10.20
+        val number = strNumber.replace(",".toRegex(), ".")
+        return if (isNumeric(number)) number.toDouble() else 0.0
+    }
+
+    private fun isNumeric(strNumber: String?): Boolean {
+        if (strNumber.isNullOrBlank()) return false;
+        val number = strNumber.replace(",".toRegex(), ".")
+        return number.matches("""[-+]?[0-9]*\.?[0-9]+""".toRegex())
     }
 }
